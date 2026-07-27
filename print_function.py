22b0084357a0d10fc3f83c40396e33f8c3128df8
Anush Kothuri <heyanushkothuri@gmail.com>
2026-07-27 06:27:02 +0530

    esp

--- print_function.py
+++ print_function.py
@@ -1,5 +1,11 @@
 #ESCAPE SEQUENCES
+# 1. \n and \t
 print("""Your learning path:
 \n\t-Python Basics
 \n\t-Data engineering
 \n\t-AI""")
+# 2. \" and \"
+print("Python is \"user friendly\"")
+print("Python is \'easy\' to understand")
+print('Python is rich for "Data-Jobs"') #still double quotes
+print("Python ")
