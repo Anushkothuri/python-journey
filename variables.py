b364af264ee0a89fa44b96c2067c0ebf9b7e8c83
Anush Kothuri <kothurianush16@gmail.com>
2026-07-27 20:46:36 +0530

    variables

--- variables.py
+++ variables.py
@@ -1,5 +1,5 @@
 #VARIABLES
-#Here we will change name affects everywhere name variable exists
+#Here we will change 'name' affects everywhere 'name' variable exists
 name="Anush" 
 print("My name is", name)
 print(name, "is a video editor")
