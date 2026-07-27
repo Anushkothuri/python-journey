7ccae27f0f88c70e448b31f6377bd46884502871
Anush Kothuri <kothurianush16@gmail.com>
2026-07-27 19:42:56 +0530

    prbsolving1

---
+++ problem_solving_1.py
@@ -0,0 +1,16 @@
+# PROBLEM 1
+print("Name : Anush")
+print("Age:20")
+print("Dream : Become a software engineer")
+# PROBLEM 2
+a=10
+b=25
+sum=a+b
+print("sum: ",sum)
+# PROBLEM 3
+city="eluru"
+country="india"
+language="python"
+print(city)
+print(country)
+print(language)
