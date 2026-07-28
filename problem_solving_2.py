63b2fe5752cb6432e129d9780dccdf0e7093989e
Anush Kothuri <kothurianush16@gmail.com>
2026-07-28 06:52:19 +0530

    solved

--- problem_solving_2.py
+++ problem_solving_2.py
@@ -1,3 +1,4 @@
+#PROBLEM 1
 print("----- MY PROFILE -----")
 print()
 input("Name : ")
@@ -5,3 +6,23 @@ input("Age : ")
 input("City : ")
 print()
 print("----------------------")
+#PROBLEM 2
+Fav_Food=input("Fav Food : ")
+Fav_Movie=input("Fav Movie : ")
+Fav_Lang=input("Fav Programming Lang : ")
+print("your favourites")
+print(Fav_Food)
+print(Fav_Movie)
+print(Fav_Lang)
+#PROBLEM 3
+name=input("Enter your name: ")
+college=input("Enter college name: ")
+branch=input("enter branch : ")
+year=input("enter year : ")
+print("""====================
+   STUDENT CARD
+====================""")
+print("Name    : ",name)
+print("College : ",college)
+print("Branch  : ",branch)
+print("Year    : ",year)
