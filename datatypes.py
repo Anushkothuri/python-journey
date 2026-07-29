89983225b6cbd05e842a8e3418ddb34fd6376eda
Anush Kothuri <kothurianush16@gmail.com>
2026-07-29 06:38:01 +0530

    datatype

---
+++ datatypes.py
@@ -0,0 +1,23 @@
+#Datatypes
+a = 10         #int
+b = 3.14       #float
+c = "Python"   #str
+d = 'Python'   #str
+e = "1234"     #str
+f = True       #bool
+g = False      #bool
+h = None       #None Type
+i = ""         #str - blank
+j = " "        #str - empty space
+print(a,b,c,d,e,f,g,h,i,j)
+#Showing type
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+print(type(i))
+print(type(j))
