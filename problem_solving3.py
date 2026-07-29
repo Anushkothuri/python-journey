aaab759c9778056424ebc4ae5134fc71b4b16beb
Anush Kothuri <kothurianush16@gmail.com>
2026-07-29 21:09:26 +0530

    type

--- problem_solving3.py
+++ problem_solving3.py
@@ -9,3 +9,17 @@ print(b)
 print(c)
 print(d)
 print(e)
+
+#PROBLEM 2
+name = "Anush"
+age = 20
+salary = 15000.50
+is_editor = True
+print("Value: ",name)
+print(type(name))
+print("Value: ",age)
+print(type(age))
+print("Value: ",salary)
+print(type(salary))
+print("Value: ",is_editor)
+print(type(is_editor))
