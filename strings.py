b21c8cc962f8229f31c42438023070803856429c
Anush Kothuri <kothurianush16@gmail.com>
2026-07-30 20:45:04 +0530

    count

--- strings.py
+++ strings.py
@@ -2,8 +2,23 @@
 #type()
 name="Anush"
 print(type(name))
+print() #line gap
 #str()
 age=21
 print("Age is: "+str(age)) #int converts to str
 age=age+1
 print(age)
+print() #line gap
+#len()
+id="anush"
+print(len(id))
+if len(id)<8:
+    print("Invalid")
+print()
+#count()
+text=""" 
+Python is easy to learn.
+I love python.
+Python is powerful!
+"""
+print(text.count("Python"))
