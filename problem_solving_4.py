442f22bf68120a516e7f051308a87cc42da4bd97
Anush Kothuri <kothurianush16@gmail.com>
2026-07-31 06:22:34 +0530

    prbsol

--- problem_solving_4.py
+++ problem_solving_4.py
@@ -1,2 +1,10 @@
+#PROBLEM 1
+#replace()
 num="+49 (176) 123-4567"
 print(num.replace("+","00").replace("(","").replace(")","").replace("-",""))
+#PROBLEM 2
+#plus(+)
+file="C:/Users/Anush/"
+folder="reports"
+full_file_path=file+folder
+print(full_file_path)
