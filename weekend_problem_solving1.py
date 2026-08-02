f702cb8d8357dfd4cdf9d0d641c07a6d733ac0ca
Anush Kothuri <kothurianush16@gmail.com>
2026-08-02 13:54:05 +0530

    weekendprblm

---
+++ weekend_problem_solving1.py
@@ -0,0 +1,10 @@
+#Practicing all the concepts of 1st Week!
+#Problem 1
+name=input("Enter your name :")
+age=input("Enter age :")
+city=input("city :")
+profession=input("profession: ")
+print(name)
+print(age)
+print(city)
+print(profession)
