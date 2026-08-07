1295904b975d4ae9196e4aba6f434b514b99cd52
Anush Kothuri <kothurianush16@gmail.com>
2026-08-07 06:24:21 +0530

    elif

--- conditional_statements.py
+++ conditional_statements.py
@@ -9,3 +9,15 @@ if score >= 90:
     print("Grade A")
 else:
     print("Fail")
+print()
+#elif
+score = int(input("Enter score: "))
+if score >= 90:
+    print("Grade A")
+elif score>=80:
+    print("Grade B")
+elif score>=70:
+    print("Grade C")
+else:
+    print("Fail")
+print()
